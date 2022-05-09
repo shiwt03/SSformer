8e24443c0979be6c5f1f0c98c36d04a9ca6ac599
shiwt03 <shiwt2017@outlook.com>
2022-05-09 13:56:51 +0800

    main content

---
+++ configs/SSformer/SSformer_swin_768x768_160k_Cityscapes.py
@@ -0,0 +1,74 @@
+_base_ = [
+    '../_base_/datasets/cityscapes_768x768.py',
+    '../_base_/default_runtime.py', 
+    '../_base_/schedules/schedule_160k.py'
+]
+norm_cfg = dict(type='BN', requires_grad=True)
+backbone_norm_cfg = dict(type='LN', requires_grad=True)
+model = dict(
+    type='EncoderDecoder',
+    pretrained='pretrain/swin_base_patch4_window7_224_22k.pth',
+    # pretrained=None,
+    backbone=dict(
+        type='SwinTransformer',
+        pretrain_img_size=224,
+        embed_dims=128,
+        patch_size=4,
+        window_size=7,
+        mlp_ratio=4,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        strides=(4, 2, 2, 2),
+        out_indices=(0, 1, 2, 3),
+        qkv_bias=True,
+        qk_scale=None,
+        patch_norm=True,
+        drop_rate=0.,
+        attn_drop_rate=0.,
+        drop_path_rate=0.3,
+        use_abs_pos_embed=False,
+        act_cfg=dict(type='GELU'),
+        norm_cfg=backbone_norm_cfg),
+
+    decode_head=dict(
+        type='SegformerHead',
+        in_channels=[128, 256, 512, 1024],
+        in_index=[0, 1, 2, 3],
+        channels=256,
+        dropout_ratio=0.1,
+        num_classes=19,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+    
+    train_cfg=dict(),
+    test_cfg=dict(mode='whole'))
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=8,
+)
